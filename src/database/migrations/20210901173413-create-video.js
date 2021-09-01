d38ccc267c3c9b50d62489830b9ea47d0b7010d0
Gautier Lapeyronie <goochlapeyronie@gmail.com>
2021-09-01 19:40:47 +0200

    add model Video

---
+++ src/database/migrations/20210901173413-create-video.js
@@ -0,0 +1,33 @@
+'use strict';
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('Videos', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    url: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('Videos');
+}
---
+++ src/models/Video.js
@@ -0,0 +1,26 @@
+'use strict';
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class Video extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Video.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      url: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Video'
+    }
+  );
+  return Video;
+};
